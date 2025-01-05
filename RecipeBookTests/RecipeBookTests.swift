f4917309678902b8dd39895642952e6dab1c93ae
d-rummo <d.rummo@wemakefab.ru>
2025-01-05 14:49:31 +0300

    Initial Commit

---
+++ RecipeBookTests/RecipeBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookTests.swift
+//  RecipeBookTests
+//
+//  Created by Daniil on 5.01.25.
+//
+
+import Testing
+@testable import RecipeBook
+
+struct RecipeBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
